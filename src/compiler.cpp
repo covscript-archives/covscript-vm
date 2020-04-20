759ec931e2a8a385e9e22ddb83e8fb4ec33a4583
imkiva <libkernelpanic@gmail.com>
2020-04-20 23:17:44 +0800

    compiler: finish rust bridge

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -2,18 +2,37 @@
 #include "rustlib.hpp"
 
 namespace cs {
+    static void check_and_throw_error(CCompileResult &result) {
+        if (result.kind != CCompileResultKind::Success) {
+            if (result.kind != CCompileResultKind::MallocError) {
+                std::string message{result.message};
+                std::free(reinterpret_cast<void *>(
+                    const_cast<char *>(result.message)));
+                mpp::throw_ex<compile_error>(message);
+            } else {
+                mpp::throw_ex<compile_error>("An error occurred but we failed to "
+                                             "save the error message because malloc() "
+                                             "returned a nullptr");
+            }
+        }
+    }
+
     std::unique_ptr<compiled_code> compiler::compile_to_bytecode(const std::string &file,
                                                                  const std::string &src) {
         auto result = ::compile_source(file.c_str(), src.c_str());
-        if (result.kind != CCompileResultKind::Success) {
-            mpp::throw_ex<compile_error>(result.message);
-        }
+        check_and_throw_error(result);
+
         return std::make_unique<compiled_code>(
             const_cast<unsigned char *>(result.compiled_code),
             result.compiled_code_length);
     }
 
     std::string compiler::compile_to_ast(const std::string &file, const std::string &src) {
-        return std::__cxx11::string();
+        auto result = ::compile_to_ast(file.c_str(), src.c_str());
+        check_and_throw_error(result);
+        std::string ast_text{result.message};
+        std::free(reinterpret_cast<void *>(
+            const_cast<char *>(result.message)));
+        return std::move(ast_text);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
-#include <rustlib.hpp>
+#include <csvm/compiler.hpp>
+#include <cstdio>
+#include <cerrno>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, const char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <INPUT>\n", argv[0]);
+        return 1;
+    }
+
+    std::fstream fin(argv[1], std::ios::in);
+    if (!fin) {
+        fprintf(stderr, "%s: cannot open: %s\n", argv[1],
+            strerror(errno));
+        return 1;
+    }
+
+    std::string src;
+    std::string line;
+    while (std::getline(fin, line)) {
+        src.append(line);
+        src.push_back('\n');
+    }
+
+    try {
+        auto ast = cs::compiler::compile_to_ast(argv[1], src);
+        printf("%s\n", ast.c_str());
+    } catch (cs::compile_error &err) {
+        printf("CompileError: %s\n", err.what());
+    }
     return 0;
 }
